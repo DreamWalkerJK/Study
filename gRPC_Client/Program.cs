0b377075a66f6432e9d4d5d520250ca99a2dcf99
DreamWalkerJK <lunatic_jk@qq.com>
2023-02-28 16:17:37 +0800

    2023/2/28 gRPC Service&Client Demo

---
+++ gRPC_Client/Program.cs
@@ -0,0 +1,18 @@
+﻿using Grpc.Net.Client;
+
+namespace gRPC_Client
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using var channel = GrpcChannel.ForAddress("https://localhost:7129");
+            var client = new Greeter.GreeterClient(channel);
+            var reply = await client.SayHelloAsync(
+                new HelloRequest { Name = "gRPC_Client" });
+
+            Console.WriteLine($"Greeting: {reply.Message}");
+            Console.ReadKey();
+        }
+    }
+}
