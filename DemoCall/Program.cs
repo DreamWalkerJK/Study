9404ceedb244db33a22cc5bf2e609b2f7ca5f338
DreamWalkerJK <lunatic_jk@qq.com>
2023-03-15 22:38:19 +0800

    2023/3/15 webservice 创建、发布、使用 demo

---
+++ DemoCall/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using WebDemo.DemoWebReference;
+
+namespace DemoCall
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var webService = new DemoService();
+            var hiJerry = webService.GetHi("jerry");
+            var addResult = webService.Add(6, 8);
+            var subResult = webService.Sub(9, 3);
+            var mulResult = webService.Mul(7, 3);
+            var divResult = webService.div(4, 2);
+
+            Console.WriteLine(hiJerry);
+            Console.WriteLine($"add:{addResult}");
+            Console.WriteLine($"sub:{subResult}");
+            Console.WriteLine($"mul:{mulResult}");
+            Console.WriteLine($"div:{divResult}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ WebDemo/DemoService.asmx.cs
@@ -0,0 +1,46 @@
+﻿using System.Web.Services;
+
+namespace WebDemo
+{
+    /// <summary>
+    /// Summary description for DemoService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class DemoService : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string GetHi(string name)
+        {
+            return $"Hi,{name}";
+        }
+
+        [WebMethod]
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        [WebMethod]
+        public double Sub(double a, double b)
+        {
+            return a - b;
+        }
+
+        [WebMethod]
+        public double Mul(double a, double b)
+        {
+            return a * b;
+        }
+
+        [WebMethod]
+        public double div(double a, double b)
+        {
+            return a / b;
+        }
+    }
+}
